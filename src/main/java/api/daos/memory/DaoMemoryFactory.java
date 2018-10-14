0b1cf652b29de34ed2830b40ef61dfae791516f4
Alex del Puerto <alejandro.delpuerto2@gmail.com>
2018-10-14 18:29:07 +0200

    Added songDaoMemory

--- src/main/java/api/daos/memory/DaoMemoryFactory.java
+++ src/main/java/api/daos/memory/DaoMemoryFactory.java
@@ -2,10 +2,12 @@ package api.daos.memory;
 
 import api.daos.DaoFactory;
 import api.daos.PersonDao;
+import api.daos.SongDao;
 
 public class DaoMemoryFactory extends DaoFactory {
 
     private PersonDao personDao;
+    private SongDao songDao;
 
     @Override
     public PersonDao getPersonDao() {
@@ -13,5 +15,13 @@ public class DaoMemoryFactory extends DaoFactory {
             this.personDao = new PersonDaoMemory();
         }
         return this.personDao;
+    }
+
+    @Override
+    public SongDao getSongDao() {
+        if (this.songDao == null) {
+            this.songDao = new SongDaoMemory();
+        }
+        return this.songDao;
     }
 }
